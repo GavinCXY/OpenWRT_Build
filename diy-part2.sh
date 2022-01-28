a5fdf7e062ab603f426afa2a1e486a33b671f58d
GavinCXY <98086322+GavinCXY@users.noreply.github.com>
2022-01-28 21:57:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,12 +26,12 @@
 ./scripts/feeds install -f luci-app-dockerman
 
 
-wget --show-progress https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz00
-wget --show-progress https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz01
-wget --show-progress https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz02
-wget --show-progress https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz03
-wget --show-progress https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz04
-wget --show-progress https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/staging_dir.tar.xz
+wget https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz00 &
+wget https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz01 &
+wget https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz02 &
+wget https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz03
+wget https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/build_dir.tar.xz04
+wget https://github.com/GavinCXY/OpenWRT_Build/releases/download/0.0.1/staging_dir.tar.xz
 
 tar -Jxvf staging_dir.tar &
 
